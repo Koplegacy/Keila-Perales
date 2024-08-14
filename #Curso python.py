e151e5d84daa00505065ffd3d50f0e99260a783a
Koplegacy <keila.jareth1@gmail.com>
2024-08-14 02:40:51 -0600

    Initial commit

---
+++ #Curso python.py
@@ -0,0 +1,25 @@
+#Curso python
+
+#Día 1
+
+#Funcion print
+print ("Hola mundo te digo 'Hola'")
+print (100+50+16)
+
+
+#Funcion de los strings
+print("Hola"+" Keila")
+print("Hola \"Keila\"")
+print("Hola Keila, ¿cómo estás?\nespero que bien")
+print('hey how´s going on?\t Let´s go to dance')
+
+#funcion Input
+input("Nombre:")
+input("Apellido:")
+print(input("Dime tu nombre:"))
+print("Tu nombre es "+input("Dime tu nombre:"))
+
+#elegir nombre para cerveza
+
+print("El nombre de tu cerveza es:"+input("Tipo de cerveza que vendes(clara/negra):")+" "+input("Elije una estación del año:"))
+
